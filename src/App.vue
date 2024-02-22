dc649040fdccdeb84d084160f32894e037d80ad8
Gabulon <gabo.fajardo7@gmail.com>
2024-02-22 16:50:39 -0600

    Como funciona <style> en los single file components? que es el <style scoped> y más

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <header>
+            <h1>Planificador de Gastos</h1>
+        </header>
+    </div>
+
+</template>
+
+<style  >
+
+</style>
---
+++ src/components/Filtro.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <header>
+            <h2>Filtros</h2>
+        </header>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
