f6ca2fb753a7c46fd61b8d89a3d6dec1ab45fd81
Gabulon <gabo.fajardo7@gmail.com>
2024-02-29 20:11:36 -0600

    Añadiendo un boton para Nuevos Gastos

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import {ref} from 'vue'
 import Presupuesto from "./components/Presupuesto.vue";
 import ControlPresupuesto from './components/ControlPresupuesto.vue'
 
+import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
 const presupuesto=ref(0)
 
 const disponible=ref(0)
@@ -30,6 +32,14 @@ const definirPresupuesto =(cantidad) =>{
         />
       </div>
     </header>
+    <main v-if="presupuesto > 0">
+      <div class="crear-gasto">
+          <img 
+          :src="iconoNuevoGasto"
+          alt="icono nuevo gasto"
+          />
+      </div>
+    </main>
   </div>
 </template>
 
@@ -87,4 +97,17 @@ background-color: var(--blanco);
 border-radius: 1.2rem;
 padding: 5rem;
 }
+.crear-gasto{
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+}
+.crear-gasto img{
+  width: 5 rem;
+  cursor: pointer;
+}
+.crear-gasto img:hover{
+  cursor: pointer;
+}
+
 </style>
