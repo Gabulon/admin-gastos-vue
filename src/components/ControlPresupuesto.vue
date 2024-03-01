578090c421c0ac4f7154a27fd12703593c9c2898
Gabulon <gabo.fajardo7@gmail.com>
2024-02-29 19:28:26 -0600

    CSS AL Panel de Presupuesto

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -9,7 +9,7 @@ import imagen from'../assets/img/grafico.jpg'
             />
         </div>
         <div class="contenedor-presupuesto">
-            <button class="reset-pp">
+            <button class="reset-app">
                 Resetear App
             </button>
             <p>
@@ -29,3 +29,57 @@ import imagen from'../assets/img/grafico.jpg'
 
     </div>
 </template>
+<style scoped>
+.dos-columnas{
+    display: flex;
+    flex-direction: column;
+}
+.dos-columnas:first-child{
+    margin-bottom: 3rem;
+}
+    @media (min-width: 768px) {
+        .dos-columnas{
+            flex-direction: row;
+            gap:4rem;
+            align-items: center;
+        }
+        .dos-columnas:first-child{
+            margin-bottom: 3rem;
+        }
+        
+    }
+.contenedor-presupuesto{
+    width: 100%;
+
+}
+.reset-app{
+    background-color: #DB2777;
+    border: none;
+    padding: 1rem;
+    width: 100%;
+    color: var(--blanco);
+    font-weight: 900;
+    text-transform: uppercase;
+    border-radius: 1rem;
+    transition-property: background-color;
+    transition-duration: 300ms;
+}
+.reset-app:hover{
+    cursor: pointer;
+    background-color: #c11d67;
+}
+.contenedor-presupuesto p {
+    font-size: 2.4rem;
+    text-align: center;
+}
+@media (min-width: 768px) {
+    .contenedor-presupuesto p {
+        font-size: 2.4rem;
+        text-align: left;
+    }
+}
+.contenedor-presupuesto span {
+    font-weight: 900;
+    color: var(--azul);
+}
+</style>
