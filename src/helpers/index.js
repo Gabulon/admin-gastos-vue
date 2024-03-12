460ec7dcb02479d882020a7a6b9b4849aa0b80d6
Gabulon <gabo.fajardo7@gmail.com>
2024-03-12 10:43:39 -0600

    Generando un ID unico sin dependencias

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -4,3 +4,8 @@ return Number(cantidad).toLocaleString('es-US',{
     currency:'USD'
 })
 }
+export const generarId = () =>{
+    const fecha=Date.now().toString(36)
+    const random = Math.random().toString(36).substring(2)
+    return random + fecha
+}
