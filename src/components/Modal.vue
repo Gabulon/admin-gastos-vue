42c6e25d5b00693bb52e46162decf374e3aaf9c0
Gabulon <gabo.fajardo7@gmail.com>
2024-03-29 12:44:43 -0600

    Ajustando el calculo del presupuesto

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,184 +1,203 @@
 <script setup>
-import {ref} from 'vue'
-import Alerta from './Alerta.vue';
-import cerrarModal from '../assets/img/cerrar.svg'
+import { ref } from "vue";
+import Alerta from "./Alerta.vue";
+import cerrarModal from "../assets/img/cerrar.svg";
 
-const error=ref('')
+const error = ref("");
 
-const emit = defineEmits(['ocultar-modal','guardar-gasto','update:nombre','update:cantidad','update:categoria'])
-const props =defineProps({
-    modal:{
-        type:Object,
-        required:true
-    },
-    nombre:{
-        type:String,
-        required:true,
-    },
-    cantidad:{
-        type:[String,Number],
-        required:true,
-    },
-    categoria:{
-        type:String,
-        required:true,
-    },
-    disponible:{
-        type: Number,
-        required:true
-    }
-})
-const agregarGasto=()=>{
-    const {nombre,cantidad,categoria,disponible}=props
-    if([nombre,cantidad,categoria].includes('')){
-        error.value='Todos los campos son obligatorios'
-        setTimeout(()=>{
-            error.value=''
-        },3000);
+const emit = defineEmits([
+  "ocultar-modal",
+  "guardar-gasto",
+  "update:nombre",
+  "update:cantidad",
+  "update:categoria",
+]);
+const props = defineProps({
+  modal: {
+    type: Object,
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  cantidad: {
+    type: [String, Number],
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  disponible: {
+    type: Number,
+    required: true,
+  },
+  id: {
+    type: [String, null],
+    required: true,
+  },
+});
 
-        return
-    }
-    if(cantidad<=0){
-        error.value ='Cantidad no válida'
-        setTimeout(()=>{
-            error.value=''
-        },3000);
-        return
-    }
-    //validar el usuario no gaste mas de lo disponible
-    if(cantidad>disponible){
-        error.value ='Has excedido el Presupuesto'
-        setTimeout(()=>{
-            error.value=''
-        },3000);
-        return
+const old =props.cantidad
+
+const agregarGasto = () => {
+  const { nombre, cantidad, categoria, disponible, id } = props;
+  if ([nombre, cantidad, categoria].includes("")) {
+    error.value = "Todos los campos son obligatorios";
+    setTimeout(() => {
+      error.value = "";
+    }, 3000);
+
+    return;
+  }
+  if (cantidad <= 0) {
+    error.value = "Cantidad no válida";
+    setTimeout(() => {
+      error.value = "";
+    }, 3000);
+    return;
+  }
+  //validar el usuario no gaste mas de lo disponible
+  if (id) {
+    if(cantidad>old+disponible){
+        error.value = "Has excedido el Presupuesto";
+      setTimeout(() => {
+        error.value = "";
+      }, 3000)
+      return
     }
 
-    emit('guardar-gasto')
-}
+  } else {
+    if (cantidad > disponible) {
+      error.value = "Has excedido el Presupuesto";
+      setTimeout(() => {
+        error.value = "";
+      }, 3000)
+      return
+    }
+  }
+  emit("guardar-gasto");
+};
 </script>
 <template>
-    <div class="modal">
-        <div class="cerrar-modal">
-            <img
-            :src="cerrarModal"
-            @click="$event =>$emit('ocultar-modal')"
-            />
-          
-        </div>
-        <div class="contenedor contenedor-formulario"
-        :class="[modal.animar ? 'animar' : 'cerrar']">
-        <form
-        class="nuevo-gasto"
-        @submit.prevent="agregarGasto"
-        >
-                <legend>Añadir Gastos</legend>
-                <Alerta v-if="error">{{ error }}</Alerta>
-                    <div class="campo">
-                        <label for="nombre">Nombre del Gasto</label>
-                        <input
-                        type="text"
-                        id="nombre"
-                        placeholder="Añade el nombre del Gasto"
-                        :value="nombre"
-                        @input="$emit('update:nombre',$event.target.value)"
-                        />
-
-                    </div>
-                    <div class="campo">
-                        <label for="cantidad">Cantidad</label>
-                        <input
-                        type="number"
-                        id="cantidad"
-                        placeholder="Añade la cantidad de gasto, ej.300"
-                        :value="cantidad"
-                        @input="$emit('update:cantidad',+ $event.target.value)"
-                        />
-
-                    </div>
-                    <div class="campo">
-                        <label for="categoria">Categoria</label>
-                        <select id="categoria" :value="categoria" @input="$emit('update:categoria',$event.target.value)">
-                            <option value="">-- Seleccione --</option>
-                            <option value="ahorro">Ahorro</option>
-                            <option value="comida">Comida</option>
-                            <option value="casa">Casa</option>
-                            <option value="gastos">Gastos Varios</option>
-                            <option value="ocio">Ocio</option>
-                            <option value="salud">Salud</option>
-                            <option value="subscripciones">Subscripciones</option>
-                        </select>
-                    </div>
-                    <input type="submit" value="Añadir Gasto"/>
-            </form>
-        </div>
+  <div class="modal">
+    <div class="cerrar-modal">
+      <img :src="cerrarModal" @click="($event) => $emit('ocultar-modal')" />
     </div>
+    <div
+      class="contenedor contenedor-formulario"
+      :class="[modal.animar ? 'animar' : 'cerrar']"
+    >
+      <form class="nuevo-gasto" @submit.prevent="agregarGasto">
+        <legend>Añadir Gastos</legend>
+        <Alerta v-if="error">{{ error }}</Alerta>
+        <div class="campo">
+          <label for="nombre">Nombre del Gasto</label>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Añade el nombre del Gasto"
+            :value="nombre"
+            @input="$emit('update:nombre', $event.target.value)"
+          />
+        </div>
+        <div class="campo">
+          <label for="cantidad">Cantidad</label>
+          <input
+            type="number"
+            id="cantidad"
+            placeholder="Añade la cantidad de gasto, ej.300"
+            :value="cantidad"
+            @input="$emit('update:cantidad', +$event.target.value)"
+          />
+        </div>
+        <div class="campo">
+          <label for="categoria">Categoria</label>
+          <select
+            id="categoria"
+            :value="categoria"
+            @input="$emit('update:categoria', $event.target.value)"
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="subscripciones">Subscripciones</option>
+          </select>
+        </div>
+        <input type="submit" value="Añadir Gasto" />
+      </form>
+    </div>
+  </div>
 </template>
 <style scoped>
 .modal {
-    position: absolute;
-    background-color: rgb(0 0 0 / 0.9);
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+  position: absolute;
+  background-color: rgb(0 0 0 / 0.9);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 .cerrar-modal {
-    position: absolute;
-    right: 3rem;
-    top: 3rem;
+  position: absolute;
+  right: 3rem;
+  top: 3rem;
 }
 .cerrar-modal img {
-    width: 3rem;
-    cursor: pointer;
+  width: 3rem;
+  cursor: pointer;
 }
 
 .contenedor-formulario {
-    transition-property: all;
-    transition-duration: 300ms;
-    transition-timing-function: ease-in;
-    opacity: 0;
+  transition-property: all;
+  transition-duration: 300ms;
+  transition-timing-function: ease-in;
+  opacity: 0;
 }
 .contenedor-formulario.animar {
-    opacity: 1;
+  opacity: 1;
 }
 
 .contenedor-formulario.cerrar {
-    opacity: 0;
+  opacity: 0;
 }
 
 .nuevo-gasto {
-    margin: 10rem auto 0 auto;
-    display: grid;
-    gap: 2rem;
+  margin: 10rem auto 0 auto;
+  display: grid;
+  gap: 2rem;
 }
 .nuevo-gasto legend {
-    text-align: center;
-    color: var(--blanco);
-    font-size: 3rem;
-    font-weight: 700;
+  text-align: center;
+  color: var(--blanco);
+  font-size: 3rem;
+  font-weight: 700;
 }
 .campo {
-    display: grid;
-    gap: 2rem;
+  display: grid;
+  gap: 2rem;
 }
 .nuevo-gasto input,
 .nuevo-gasto select {
-    background-color: var(--gris-claro);
-    border-radius: 1rem;
-    padding: 1rem;
-    border: none;
-    font-size: 2.2rem;
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
 }
 .nuevo-gasto label {
-    color: var(--blanco);
-    font-size: 3rem;
+  color: var(--blanco);
+  font-size: 3rem;
 }
 .nuevo-gasto input[type="submit"] {
-    background-color: var(--azul);
-    color: var(--blanco);
-    font-weight: 700;
-    cursor: pointer;
+  background-color: var(--azul);
+  color: var(--blanco);
+  font-weight: 700;
+  cursor: pointer;
 }
-
 </style>
